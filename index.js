1f8aba0092761342207b38dd6c0f65705e2c1d6e
kirill <kirill@Mac-mini-kirill.local>
2019-12-16 15:17:36 +0200

    2.mp4

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const cardRoutes = require('./routes/card')
 const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
 
-
-const MONGODB_URI = 'mongodb+srv://sotnikov_k:OP1imCvCqLmcrQri@mongovue-voe6y.mongodb.net/trainingVladilenSession?retryWrites=true&w=majority'
-
+const keys = require('./keys')
 
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
@@ -32,7 +30,7 @@ const hbs = exphbs.create({
 
 const store = new MongoStore({
   collection: 'sessions',
-  uri: MONGODB_URI
+  uri: keys.MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -52,7 +50,7 @@ app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
-  secret: 'some secret value',
+  secret: keys.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store
@@ -73,7 +71,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useFindAndModify: false,
       useNewUrlParser: true,
       useUnifiedTopology: true
---
+++ keys/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://sotnikov_k:OP1imCvCqLmcrQri@mongovue-voe6y.mongodb.net/trainingVladilenSession?retryWrites=true&w=majority',
+  SESSION_SECRET: 'some secret value'
+}
